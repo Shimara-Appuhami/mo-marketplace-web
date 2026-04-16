7f89f42dd7065993d70f1b215a867645f8a8b556
Shimara-Appuhami <shimarailshani@gmail.com>
2026-04-16 09:27:12 +0530

    defined properties to user login

---
+++ src/types/index.ts
@@ -0,0 +1,25 @@
+export interface User {
+  id: string;
+  email: string;
+  name: string;
+}
+
+export interface AuthResponse {
+  accessToken: string;
+  user: User;
+}
+
+export interface LoginInput {
+  email: string;
+  password: string;
+}
+
+export interface RegisterInput extends LoginInput {
+  name: string;
+}
+
+export interface ApiErrorResponse {
+  message?: string | string[];
+  error?: string;
+  statusCode?: number;
+}
